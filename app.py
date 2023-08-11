7f9dc9ca97c24aa8b688d754d74913a1daefde63
Will <will.maples@claytonhomes.com>
2023-08-11 11:51:02 -0400

    final flask app

--- app.py
+++ app.py
@@ -1,28 +1,32 @@
 from flask import Flask, render_template, request
-from flask_sqlalchemy import SQLAlchemy
+import polars as pl
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'  # Using SQLite database
-db = SQLAlchemy(app)
 
-class Entry(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    field1 = db.Column(db.String(255))
-    field2 = db.Column(db.String(255))
+df = pl.read_parquet('oracle_data_dictionary.parquet')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        field1 = request.form['field1']
-        field2 = request.form['field2']
-        entry = Entry(field1=field1, field2=field2)
-        db.session.add(entry)
-        db.session.commit()
-
-    entries = Entry.query.all()
-    return render_template('index.html', entries=entries)
+        table_1 = request.form['table_1']
+        table_2 = request.form['table_2']
+        if table_1 and table_2:
+            filtered_df = df.filter(pl.col('RELATIONSHIPS').str.contains(table_1))
+            filtered_df = filtered_df.filter(pl.col('RELATIONSHIPS').str.contains(table_2))
+            matching_columns = set(filtered_df['COLUMN_NAME'].to_list())
+            if matching_columns:
+                matching_columns_str = ', '.join(matching_columns)
+                return render_template('result.html', matching_columns=matching_columns_str, table_1=table_1, table_2=table_2)
+            else:
+                return render_template('result.html', message='No matching columns found.', table_1=table_1, table_2=table_2)
+        else:
+            return render_template('result.html', message='Please enter both table names.')
+    else:
+        return render_template('index.html')
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
+
+# ! search feature on TABLE_NAME
+# ! search feature on COLUMN_NAME
+# ! fuzzy matching on COLUMN_NAME
